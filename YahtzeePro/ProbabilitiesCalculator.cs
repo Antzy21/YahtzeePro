d7957d01d329e7d4af49e4a48e74044712a13503
Anthony Dunford <Anthony@Dunford.uk.com>
2023-07-30 18:12:00 +0100

    Add Probabilities calculator

---
+++ YahtzeePro/ProbabilitiesCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace YahtzeePro
+{
+    public static class ProbabilitiesCalculator
+    {
+        public static double FailProbability(int diceCount)
+        {
+            return Math.Pow(4d / 6d, (double)diceCount);
+        }
+    }
+}
