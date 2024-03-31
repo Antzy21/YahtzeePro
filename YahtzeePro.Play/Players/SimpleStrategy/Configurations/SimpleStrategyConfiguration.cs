d02a21f0c6ca8b5ee8ae0314bbf0b9fcda8bf956
Anthony Dunford <trashzy21@gmail.com>
2024-03-31 22:04:34 +0100

    Use json for simple strategy

--- YahtzeePro.Play/Players/SimpleStrategy/Configurations/SimpleStrategyConfiguration.cs
+++ YahtzeePro.Play/Players/SimpleStrategy/Configurations/SimpleStrategyConfiguration.cs
@@ -6,6 +6,5 @@ namespace YahtzeePro.Play;
 [Serializable]
 public class SimpleStrategyConfiguration
 {
-    public readonly string Name;
     public readonly Dictionary<int, int> WhenToBankWithNumberOfDice;
 }
---
+++ YahtzeePro.Play/Players/SimpleStrategy/SimpleStrategy.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace YahtzeePro.Play.Players
+{
+    internal class SimpleStrategy : IPlayer
+    {
+        private SimpleStrategyConfiguration _configuration;
+
+        public SimpleStrategy(SimpleStrategyConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Name => string.Join(",", _configuration.WhenToBankWithNumberOfDice.Select(kv => $"{kv.Key}-{kv.Value}"));
+
+        public PlayChoice GetMove(GameState gs, GameConfiguration gc)
+        {
+            if (gs.CachedScore + gs.PlayerScore >= gc.WinningValue) {
+                return PlayChoice.Safe;
+            }
+
+            if (!_configuration.WhenToBankWithNumberOfDice.TryGetValue(gs.DiceToRoll, out var whenToBank)) {
+                whenToBank = int.MaxValue;
+            }
+
+            if (gs.CachedScore >= whenToBank)
+            {
+                return PlayChoice.Safe;
+            }
+            return PlayChoice.Risky;
+        }
+    }
+}
