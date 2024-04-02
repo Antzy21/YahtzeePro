6aaa4e65a62443d0137f682b863a7d035df2f1cd
Anthony Dunford <trashzy21@gmail.com>
2024-04-02 22:20:27 +0100

    Implement ICalculationManager.cs

--- YahtzeePro.Optimum/CalculationManager.cs
+++ YahtzeePro.Optimum/CalculationManager.cs
@@ -5,17 +5,33 @@ namespace YahtzeePro.Optimum;
 public class CalculationManager : ICalculationManager
 {
     private readonly IOptimumCalculator _optimumCalculator;
-
-    private readonly Queue<GameConfiguration> queue = new();
+    private readonly Queue<GameConfiguration> _queue = new();
+    private readonly SemaphoreSlim _semaphoreSlim = new(1);
 
     public CalculationManager(IOptimumCalculator optimumCalculator)
     {
         _optimumCalculator = optimumCalculator;
     }
 
+    public IEnumerable<GameConfiguration> Queue => _queue;
+
     public void QueueCalculation(GameConfiguration gc)
     {
-        if (!queue.Contains(gc))
-            queue.Enqueue(gc);
+        if (!_queue.Contains(gc))
+            _queue.Enqueue(gc);
+        if (_semaphoreSlim.CurrentCount != 0)
+            Task.Run(RunCalculations);
+    }
+
+    private Task RunCalculations()
+    {
+        _semaphoreSlim.Wait();
+        while (_queue.Count > 0)
+        {
+            var gc = _queue.Dequeue();
+            _optimumCalculator.Calculate(gc);
+        }
+        _semaphoreSlim.Release();
+        return Task.CompletedTask;
     }
 }
--- YahtzeePro.Optimum/ICalculationManager.cs
+++ YahtzeePro.Optimum/ICalculationManager.cs
@@ -4,5 +4,6 @@ namespace YahtzeePro.Optimum;
 
 public interface ICalculationManager
 {
+    public IEnumerable<GameConfiguration> Queue { get; }
     public void QueueCalculation(GameConfiguration gc);
 }
