ba0da4cf7464d745e2b103abe06de6e9a025e592
Anthony Dunford <Anthony@Dunford.uk.com>
2024-11-19 20:59:46 +0000

    Remove trouble test

--- YahtzeePro.tests/Optimum/CalculationManagerTests.cs
+++ YahtzeePro.tests/Optimum/CalculationManagerTests.cs
@@ -33,41 +33,6 @@ namespace YahtzeePro.tests.Optimum
         }
         
         [Fact]
-        public void QueueCalculation_QueueingTwo_CallsCalculateOnceThenTwice()
-        {
-            // Arrange
-            var mockOptimumCalculator = GetMockOptimumCalculator();
-            
-            var mockOptimumRepo = new Mock<IOptimumStrategyRepository>();
-            var calculationManager = new CalculationManager(mockOptimumCalculator.Object, mockOptimumRepo.Object);
-            var gameConfiguration1 = new GameConfiguration(500, 5);
-            var gameConfiguration2 = new GameConfiguration(500, 3);
-            
-            // Act
-            calculationManager.QueueCalculation(gameConfiguration1);
-            calculationManager.QueueCalculation(gameConfiguration2);
-            
-            // Assert
-            mockOptimumCalculator.Verify(
-                m => m.Calculate(
-                    It.IsAny<GameConfiguration>(),
-                    It.IsAny<int>(),
-                    It.IsAny<int>()),
-                Times.Once()
-            );
-            
-            Thread.Sleep(150);
-            
-            mockOptimumCalculator.Verify(
-                m => m.Calculate(
-                    It.IsAny<GameConfiguration>(),
-                    It.IsAny<int>(),
-                    It.IsAny<int>()),
-                Times.Exactly(2)
-            );
-        }
-        
-        [Fact]
         public void Queue_After4UniqueItemsAreQueued_Shows3Items()
         {
             // Arrange
