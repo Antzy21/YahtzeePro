96c8d2dc0f58eb56dda891ad595fbf68af9a9c20
Anthony Dunford <Anthony@Dunford.uk.com>
2024-11-23 14:28:25 +0000

    Restructure DiceCobinationTests

--- YahtzeePro.tests/DiceCombinationTests.cs
+++ YahtzeePro.tests/DiceCombinationTests.cs
@@ -4,44 +4,48 @@ namespace YahtzeePro.tests
 {
     public class DiceCombinationTests
     {
-        [Fact]
-        public void ComboCase1()
+        [Theory]
+        [InlineData(150, 1, 2, 3, 4, 5, 6)]
+        [InlineData(200, 2, 2, 2, 4)]
+        [InlineData(600, 6, 6, 6)]
+        [InlineData(4100, 4, 4, 4, 4, 1)]
+        public void Score_IsCalculatedCorrectly(int expectedScore, params int[] dice)
         {
-            var diceCombo = new DiceCombination(1, 2, 3, 4, 5, 6);
-            Assert.Equal(150, diceCombo.Score);
-            Assert.Equal(2, diceCombo.NumberOfScoringDice);
-            Assert.Equal("1,2,3,4,5,6,", diceCombo.ToString());
-            Assert.False(diceCombo.AllDiceAreScoring);
+            var diceCombo = new DiceCombination(dice);
+            Assert.Equal(expectedScore, diceCombo.Score);
+        }
+        
+        [Theory]
+        [InlineData("1,2,3,4,5,6,", 1, 2, 3, 4, 5, 6)]
+        [InlineData("2,2,2,4,", 2, 2, 2, 4)]
+        [InlineData("6,6,6,", 6, 6, 6)]
+        [InlineData("1,4,4,4,4,", 4, 4, 4, 4, 1)]
+        public void ToString_IsCorrect(string expectedString, params int[] dice)
+        {
+            var diceCombo = new DiceCombination(dice);
+            Assert.Equal(expectedString, diceCombo.ToString());
         }
 
-        [Fact]
-        public void ComboCase2()
+        [Theory]
+        [InlineData(2, 1, 2, 3, 4, 5, 6)]
+        [InlineData(3, 2, 2, 2, 4)]
+        [InlineData(3, 6, 6, 6)]
+        [InlineData(5, 4, 4, 4, 4, 1)]
+        public void NumberOfScoringDice_IsCalculatedCorrectly(int expectedNumberOfScoringDice, params int[] dice)
         {
-            var diceCombo = new DiceCombination(2, 2, 2, 4);
-            Assert.Equal(200, diceCombo.Score);
-            Assert.Equal(3, diceCombo.NumberOfScoringDice);
-            Assert.Equal("2,2,2,4,", diceCombo.ToString());
-            Assert.False(diceCombo.AllDiceAreScoring);
+            var diceCombo = new DiceCombination(dice);
+            Assert.Equal(expectedNumberOfScoringDice, diceCombo.NumberOfScoringDice);
         }
 
-        [Fact]
-        public void ComboCase3()
+        [Theory]
+        [InlineData(false, 1, 2, 3, 4, 5, 6)]
+        [InlineData(false, 2, 2, 2, 4)]
+        [InlineData(true, 6, 6, 6)]
+        [InlineData(true, 4, 4, 4, 4, 1)]
+        public void AllDiceAreScoring_IsCorrect(bool expectedBool, params int[] dice)
         {
-            var diceCombo = new DiceCombination(6, 6, 6);
-            Assert.Equal(600, diceCombo.Score);
-            Assert.Equal(3, diceCombo.NumberOfScoringDice);
-            Assert.Equal("6,6,6,", diceCombo.ToString());
-            Assert.True(diceCombo.AllDiceAreScoring);
-        }
-
-        [Fact]
-        public void ComboCase4()
-        {
-            var diceCombo = new DiceCombination(4, 4, 4, 4, 1);
-            Assert.Equal(4100, diceCombo.Score);
-            Assert.Equal(5, diceCombo.NumberOfScoringDice);
-            Assert.Equal("1,4,4,4,4,", diceCombo.ToString());
-            Assert.True(diceCombo.AllDiceAreScoring);
+            var diceCombo = new DiceCombination(dice);
+            Assert.Equal(expectedBool, diceCombo.AllDiceAreScoring);
         }
     }
 }
