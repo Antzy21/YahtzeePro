27ea0828cfa90012c80ce486a5045c9c0aac255b
Anthony Dunford <trashzy21@gmail.com>
2025-05-29 10:50:42 +0100

    Setup class structure

--- YahtzeePro.Play/PlayerResolverService.cs
+++ YahtzeePro.Play/PlayerResolverService.cs
@@ -6,6 +6,21 @@ public class PlayerResolverService : IPlayerResolverService
 {
     public IAutoPlayer ResolveAutoPlayer(string playerString)
     {
+        return playerString.ToLower() switch
+        {
+            "optimum" => GetOptimumPlayer(),
+            "rolltowin" => new RollToWin(),
+            _ => ResolveSimpleStrategy(playerString),
+        };
+    }
+
+    private IAutoPlayer ResolveSimpleStrategy(string playerString)
+    {
+        throw new NotImplementedException();
+    }
+
+    private IAutoPlayer GetOptimumPlayer()
+    {
         throw new NotImplementedException();
     }
 }
